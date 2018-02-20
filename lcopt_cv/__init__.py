06ee2c1f27bc3a8471e82c2abdee05acb3562cc6
pjamesjoyce <pjamesjoyce@gmail.com>
2018-02-20 17:27:52 +0100

    first steps for gui lca generation wizard

--- lcopt_cv/__init__.py
+++ lcopt_cv/__init__.py
@@ -1,3 +1,3 @@
 from .grab_flow_chart import *
 from .heuristics import *
-#from .send_to_lcopt import LcoptWriter
+from .send_to_lcopt import LcoptWriter
--- lcopt_cv/gui.py
+++ lcopt_cv/gui.py
@@ -8,6 +8,7 @@ import numpy as np
 import cv2
 import os
 from functools import partial
+from collections import OrderedDict
 
 from .grab_flow_chart import *
 from .heuristics import *
@@ -329,7 +330,7 @@ class ImageGui:
                 self.generate_btn.config(state="disabled")
 
     def eventHandler(self, *args):
-        print(args)
+        #print(args)
         attrName = args[0]
         controlName = args[1]
 
@@ -364,11 +365,179 @@ class ImageGui:
         self.process_image()
 
     def generate_model(self):
-        messagebox.showinfo("Information", "Informative message")
+        #messagebox.showinfo("Information", "Informative message")
+        wizard = Toplevel(height=500, width=800)
+        wizard.geometry('800x800')
+        wizard.title("Generate LCA model")
+        #frame = Frame(wizard, height=500, width=800)
+        #button = Button(wizard, text="Dismiss", command=wizard.destroy)
+        #button.pack()
+        content = LcaWizard(wizard, self.IMAGEPROCESSOR_OBJECT)
+        #content.pack()
 
     def run(self):
         self.root.mainloop()
 
+class LcaWizard(Frame):
+    def __init__(self, parent, ip):
+        super().__init__(parent)
+
+        self.toplevel = parent
+
+        
+       
+
+        self.button_frame = Frame(self, bd=1, relief="raised")
+        self.content_frame = Frame(self, bg="blue")
+
+        self.back_button = Button(self.button_frame, text="<< Back", command=self.back)
+        self.next_button = Button(self.button_frame, text="Next >>", command=self.next)
+        self.finish_button = Button(self.button_frame, text="Finish", command=self.finish)
+
+        self.grid_rowconfigure(0, minsize=750, weight=1)
+        self.grid_rowconfigure(1, minsize=50, weight=0)
+        self.grid_columnconfigure(0, minsize=800, weight=0)
+
+        self.button_frame.grid(row=1, column=0, sticky=N+S+E+W)  #.pack(side="bottom", fill="x")
+        self.content_frame.grid(row=0, column=0, sticky=N+S+E+W)  #pack(side="top", fill="both", expand=True)
+        
+        self.steps = [NodeStep(self.content_frame, ip), LinkStep(self.content_frame, ip)]
+        self.current_step = 0
+        self.show_step(0)
+
+        self.pack(fill="both", expand=True)
+
+
+    def show_step(self, step):
+
+        if self.current_step is not None:
+            #remove currrent step
+            current_step = self.steps[self.current_step]
+            current_step.grid_forget()
+
+        self.current_step = step
+
+        new_step = self.steps[step]
+        new_step.grid(row=0, column=0, sticky=N+S+E+W)  #pack(side="top", fill="both", expand=True)
+
+        if step == 0:
+            self.back_button.pack_forget()
+            self.next_button.pack(side="right")
+            self.finish_button.pack_forget()
+
+        elif step == len(self.steps) - 1:
+            self.back_button.pack(side="left")
+            self.next_button.pack_forget()
+            self.finish_button.pack(side="right")
+
+        else:
+            self.back_button.pack(side="left")
+            self.next_button.pack(side="right")
+            self.finish_button.pack_forget()
+
+
+    def next(self):
+        self.show_step(self.current_step + 1)
+
+    def back(self):
+        self.show_step(self.current_step - 1)
+
+    def finish(self):
+        self.toplevel.destroy()
+
+class NodeStep(Frame):
+    def __init__(self, parent, ip):
+        super().__init__(parent)
+
+        self.config(bg="white")
+
+        Label(self, text="Hello - from NodeStep").grid(column=0, row=0)
+
+        self.node_frames = []
+        self.data = OrderedDict()
+
+        self.grid_rowconfigure(0, minsize=750, weight=1)
+        self.grid_columnconfigure(0, minsize=785, weight=1)
+
+        img_size = 150
+
+        scrollsize = img_size * len(ip.nodes)
+
+        yscrollbar = Scrollbar(self)
+        yscrollbar.grid(row=0, column=1, sticky=N+S) # pack(side="right", fill="y") 
+
+        canvas = Canvas(self, bd=0, yscrollcommand=yscrollbar.set, scrollregion=(0, 0, 800, scrollsize))
+
+        scroll_frame = Frame(canvas)
+
+        canvas.grid(row=0, column=0, sticky=N+S+E+W)
+        #canvas.pack(side="left", fill="both", expand=True)
+
+        yscrollbar.config(command=canvas.yview)
+
+        #img_size = 150
+        
+        for n, (k, node) in enumerate(ip.nodes.items()):
+
+            this_frame = Frame(scroll_frame, width=800, height=img_size)
+            image_frame = Frame(this_frame, width=200, height=img_size, relief="groove")
+            control_frame = Frame(this_frame, width=600, height=img_size, relief="groove")
+
+            box_image = convert_to_tkinter_image(ip.box_images[n], img_size)
+
+            box_img = Label(image_frame, image=box_image, relief="groove")
+            box_img.image = box_image
+            box_img.grid(column=0, row=0)
+
+            box_highlight = ip.intermediates['original'].copy()
+            (x, y, w, h) = node['coords']
+            cv2.rectangle(box_highlight, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=8)
+            #cv2.putText(box_highlight, '{}'.format(n), (int(x+5), int(y+h-5)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+
+            box_highlight_image = convert_to_tkinter_image(box_highlight, img_size)
+
+            box_highlight_img = Label(image_frame, image=box_highlight_image, relief="groove")
+            box_highlight_img.image = box_highlight_image
+            box_highlight_img.grid(column=1, row=0)
+
+            image_frame.grid(column=0, row=0)
+
+            self.data[n]={}
+
+            Label(control_frame, text="Name:").grid(column=0, row=0)
+            self.data[n]['nameEntry'] = Entry(control_frame)
+            self.data[n]['nameEntry'].grid(column=1, row=0)
+
+            Label(control_frame, text="Type:").grid(column=0, row=1)
+            self.data[n]['typeDropDown'] = Entry(control_frame)
+            self.data[n]['typeDropDown'].grid(column=1, row=1)
+            
+            Label(control_frame, text="External link:").grid(column=0, row=2)
+            self.data[n]['extLinkEntry'] = Entry(control_frame)
+            self.data[n]['extLinkEntry'].grid(column=1, row=2)
+
+
+            control_frame.grid(column=1, row=0)
+
+            self.node_frames.append(this_frame)
+
+        for n, f in enumerate(self.node_frames):
+            f.grid(column=0, row=n)
+        
+        canvas.create_window((0,0), window=scroll_frame, anchor=N+W)
+        #canvas.config(scrollregion=canvas.bbox(ALL))
+
+        
+       
+
+
+class LinkStep(Frame):
+    def __init__(self, parent, ip):
+        super().__init__(parent)
+
+        self.config(bg="red")
+
+        Label(self, text="Hello - from LinkStep").pack()
 
 if __name__ == "__main__":
 
